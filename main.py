611ec1cd9ffe7909d892d85b5cf10eef18eeea9b
AlexPuchenkov <alex12121414@mail.ru>
2022-02-15 17:20:19 +0300

    Basedata

--- main.py
+++ main.py
@@ -8,8 +8,13 @@
 # MainWindow.show()
 # sys.exit(app.exec_())
 
+import sqlite3
+import tkinter
+from tkinter import *
+from PIL import Image
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
+
 
 Form, Window = uic.loadUiType("untitled.ui")
 
@@ -19,9 +24,31 @@ form = Form()
 form.setupUi(window)
 window.show()
 
+global db
+global sql
+
+db = sqlite3.connect('person.db')
+sql = db.cursor()
+
 
 def on_c():
     print("1")
+
+
+def check_button():
+    sql.execute("""CREATE TABLE IF NOT EXISTS persons (
+        login TEXT,
+        password TEXT
+    )""")
+    db.commit()
+
+    sql.execute(f"SELECT login FROM persons WHERE login = ?", [login_user.get()])
+    if sql.fetchone() is None:
+        sql.execute(f"INSERT INTO persons VALUES('{login_user.get()}', '{password_user.get()}')")
+        db.commit()
+        print("Зарегистрировано")
+    else:
+        print("Запись уже есть")
 
 
 form.registration_window.clicked.connect(on_c)
