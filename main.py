516f6edd98188387b4d468efde950e4e4d36086e
AlexPuchenkov <alex12121414@mail.ru>
2022-04-07 09:48:43 +0300

    Create UI

--- main.py
+++ main.py
@@ -35,38 +35,22 @@ def on_c():
     print("1")
 
 
-def check_button():
-    sql.execute("""CREATE TABLE IF NOT EXISTS persons (
-        login TEXT,
-        password TEXT
-    )""")
-    db.commit()
+# def check_button():
+#     sql.execute("""CREATE TABLE IF NOT EXISTS persons (
+#         login TEXT,
+#         password TEXT
+#     )""")
+#     db.commit()
+#
+#     sql.execute(f"SELECT login FROM persons WHERE login = ?", [login_user.get()])
+#     if sql.fetchone() is None:
+#         sql.execute(f"INSERT INTO persons VALUES('{login_user.get()}', '{password_user.get()}')")
+#         db.commit()
+#         print("Зарегистрировано")
+#     else:
+#         print("Запись уже есть")
 
-    sql.execute(f"SELECT login FROM persons WHERE login = ?", [login_user.get()])
-    if sql.fetchone() is None:
-        sql.execute(f"INSERT INTO persons VALUES('{login_user.get()}', '{password_user.get()}')")
-        db.commit()
-        print("Зарегистрировано")
-    else:
-        print("Запись уже есть")
 
-
-# personal_login = input()
-# personal_password = input()
-#
-#
-# class Worker:
-#     def __init__(self, person_login, person_password):
-#         self.first_name = "Alex"
-#         self.second_name = "Alexey"
-#         self.role = "Admin"
-#         self.award = 5
-#         self.salary = 100
-#         self.additional_leave = 0
-#         self.promotion = 0
-#
-#
-# print(Worker(personal_login, personal_password).first_name)
 
 
 form.registration_window.clicked.connect(on_c)
